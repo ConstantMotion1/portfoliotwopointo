fbe4aecda1d6facb004727471b89b59942cc98ef
ConstantMotion1 <constan.fernando@hotmail.com>
2022-11-18 22:36:21 -0500

    added portfolio images

--- portfoliotwopointo/src/component/Footer.js
+++ portfoliotwopointo/src/component/Footer.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="sticky-bottom">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-    </div>
+    <div>Footer</div>
   )
 }
+
--- portfoliotwopointo/src/component/pages/Portfolio.js
+++ portfoliotwopointo/src/component/pages/Portfolio.js
@@ -1,7 +1,76 @@
-import React from 'react'
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Project1 from '../../images/musichub.png';
+import Project2 from '../../images/PJ-Loginscreen.png';
+import PassGen from '../../images/passwordgen.png';
+import TechBlog from '../../images/techblog.png';
+import WeatherApp from '../../images/weatherapp.png';
 
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Project1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Project2}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={TechBlog}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={PassGen}
+          alt="Forth slide"
+        />
+        <Carousel.Caption>
+          <h3>Forth slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={WeatherApp}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Fifth slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   )
-}
+};
+
